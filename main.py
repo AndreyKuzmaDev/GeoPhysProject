dd9eeb8500a448e10fe9e6080193377d5d3e93c1
Андрей Кузьма <andreykuzma2004@gmail.com>
2025-07-07 20:56:33 +0300

    Fix after merge

--- main.py
+++ main.py
@@ -15,135 +15,10 @@ import numpy as np
 from AppWindow import MainWindow
 import math
 import sys  # we'll need this later to run our Qt application
-import ezdxf
-from object_meshes import ObjectMesh
-from scene_objects import SceneObject
-from collisions import CollisionBox
-
 
 from object_constructors import create_cube
 from utilities import screen_pos_to_vector
 
-def load_dxf_vertices(file_path, scale=1.0, normalize=False):
-    doc = ezdxf.readfile(file_path)
-    msp = doc.modelspace()
-
-    vertices = []
-    indices = []
-    index_offset = 0
-    face_limit = 100
-    for e in msp.query('3DFACE'):
-        if len(e.wcs_vertices(False)) != 3:
-            continue
-        pts = [(vertex.x, vertex.y, vertex.z) for vertex in e.wcs_vertices(False)]
-
-        pts = np.array(pts, dtype=np.float32) * scale
-        #print(pts)
-        vertices.extend(pts)
-
-        n = len(pts)
-        for i in range(n):
-            indices.extend([(index_offset + n - i - 1) % 8 ,])
-        index_offset += n
-        face_limit -= 1
-        if face_limit == 0:
-            break
-
-    vertices = np.array(vertices, dtype=np.float32)
-
-    if normalize:
-        min_coords = vertices.min(axis=0)
-        max_coords = vertices.max(axis=0)
-        vertices = (vertices - min_coords) / (max_coords - min_coords)
-
-    indices = np.array(indices, dtype=np.uint32)
-
-    return vertices, indices
-
-
-def create_dxf_object(file_path):
-    vertices, indices = load_dxf_vertices(file_path, 1.0,True)
-    print(vertices)
-    print(indices)
-    vertVBO = vbo.VBO(np.reshape(vertices,
-                                  (1, -1)).astype(np.float32))
-    vertVBO.bind()
-# functions that create SceneObject should be defined in file object_constructors.py and imported here
-
-    colors = np.tile(np.array([0.0, 0.0, 0.0], dtype=np.float32), (len(vertices), 1))
-    colorVBO = vbo.VBO(np.reshape(colors,
-                                  (1, -1)).astype(np.float32))
-    colorVBO.bind()
-
-    print(vertVBO)
-    print(colorVBO)
-
-    mesh = ObjectMesh(vertVBO, colorVBO, indices)
-
-    min_v = vertices.min(axis=0)
-    max_v = vertices.max(axis=0)
-    collision = CollisionBox(glm.vec3(min_v), glm.vec3(max_v))
-
-    center = (min_v + max_v) / 2.0
-
-    obj = SceneObject(mesh, collision, center)
-
-    return obj
-def create_cube():
-
-
-    cubeClrArray = np.array(
-        [[0.0, 0.0, 0.0],
-         [1.0, 0.0, 0.0],
-         [1.0, 1.0, 0.0],
-         [0.0, 1.0, 0.0],
-         [0.0, 0.0, 1.0],
-         [1.0, 0.0, 1.0],
-         [1.0, 1.0, 1.0],
-         [0.0, 1.0, 1.0]])
-    colorVBO = vbo.VBO(np.reshape(cubeClrArray,
-                                  (1, -1)).astype(np.float32))
-    colorVBO.bind()
-
-    cubeIdxArray = np.array(
-        [0, 1, 2, 3,
-         3, 2, 6, 7,
-         1, 0, 4, 5,
-         2, 1, 5, 6,
-         0, 3, 7, 4,
-         7, 6, 5, 4])
-    cubeVtxArray = np.array(
-        [[0.0, 0.0, 0.0],
-         [1.0, 0.0, 0.0],
-         [1.0, 1.0, 0.0],
-         [0.0, 1.0, 0.0],
-         [0.0, 0.0, 1.0],
-         [1.0, 0.0, 1.0],
-         [1.0, 1.0, 1.0],
-         [0.0, 1.0, 1.0]])
-    vertVBO = vbo.VBO(np.reshape(cubeVtxArray,
-                                 (1, -1)).astype(np.float32))
-    vertVBO.bind()
-    cubeEdgesIdxArray = np.array(
-        [0, 1,
-         1, 2,
-         2, 3,
-         3, 0,
-         0, 4,
-         1, 5,
-         2, 6,
-         3, 7,
-         4, 5,
-         5, 6,
-         6, 7,
-         7, 4]
-    )
-
-    mesh = ObjectMesh(vertVBO, colorVBO, cubeIdxArray, cubeEdgesIdxArray)
-    collision = CollisionBox(glm.vec3([0.0, 0.0, 0.0]), glm.vec3([1.0, 1.0, 1.0]))
-    obj = SceneObject(mesh, collision, np.array([0.5, 0.5, 0.5]))
-
-    return obj
 
 # TODO : MAKE THIS SHIT LOOK BETTER
 class GLWidget(QtOpenGL.QGLWidget):
@@ -307,7 +182,7 @@ class GLWidget(QtOpenGL.QGLWidget):
             gl.glDrawElements(gl.GL_QUADS, len(obj.mesh.faces), gl.GL_UNSIGNED_INT, obj.mesh.faces)"""
         # if obj.mesh.edges is not None:
         gl.glDrawElements(gl.GL_LINES, len(obj.mesh.faces), gl.GL_UNSIGNED_INT, obj.mesh.faces)
-        gl.glDrawElements(obj.mesh.surface_mode, len(obj.mesh.surfaces), gl.GL_UNSIGNED_INT, obj.mesh.surfaces)
+        gl.glDrawElements(obj.mesh.surface_mode, len(obj.mesh.faces), gl.GL_UNSIGNED_INT, obj.mesh.faces)
         gl.glPopMatrix()
 
 
--- object_constructors.py
+++ object_constructors.py
@@ -1,3 +1,4 @@
+import ezdxf
 import numpy as np
 from OpenGL.arrays import vbo
 from pyglm import glm
@@ -8,20 +9,74 @@ from object_meshes import ObjectMesh
 from scene_objects import SceneObject
 
 
-def create_cube():
-    cubeVtxArray = np.array(
-        [[0.0, 0.0, 0.0],
-         [1.0, 0.0, 0.0],
-         [1.0, 1.0, 0.0],
-         [0.0, 1.0, 0.0],
-         [0.0, 0.0, 1.0],
-         [1.0, 0.0, 1.0],
-         [1.0, 1.0, 1.0],
-         [0.0, 1.0, 1.0]])
-    vertVBO = vbo.VBO(np.reshape(cubeVtxArray,
-                                 (1, -1)).astype(np.float32))
-    vertVBO.bind()
+def load_dxf_vertices(file_path, scale=1.0, normalize=False):
+    doc = ezdxf.readfile(file_path)
+    msp = doc.modelspace()
 
+    vertices = []
+    indices = []
+    index_offset = 0
+    face_limit = 100
+    for e in msp.query('3DFACE'):
+        if len(e.wcs_vertices(False)) != 3:
+            continue
+        pts = [(vertex.x, vertex.y, vertex.z) for vertex in e.wcs_vertices(False)]
+
+        pts = np.array(pts, dtype=np.float32) * scale
+        #print(pts)
+        vertices.extend(pts)
+
+        n = len(pts)
+        for i in range(n):
+            indices.extend([(index_offset + n - i - 1) % 8 ,])
+        index_offset += n
+        face_limit -= 1
+        if face_limit == 0:
+            break
+
+    vertices = np.array(vertices, dtype=np.float32)
+
+    if normalize:
+        min_coords = vertices.min(axis=0)
+        max_coords = vertices.max(axis=0)
+        vertices = (vertices - min_coords) / (max_coords - min_coords)
+
+    indices = np.array(indices, dtype=np.uint32)
+
+    return vertices, indices
+
+
+def create_dxf_object(file_path):
+    vertices, indices = load_dxf_vertices(file_path, 1.0,True)
+    print(vertices)
+    print(indices)
+    vertVBO = vbo.VBO(np.reshape(vertices,
+                                  (1, -1)).astype(np.float32))
+    vertVBO.bind()
+# functions that create SceneObject should be defined in file object_constructors.py and imported here
+
+    colors = np.tile(np.array([0.0, 0.0, 0.0], dtype=np.float32), (len(vertices), 1))
+    colorVBO = vbo.VBO(np.reshape(colors,
+                                  (1, -1)).astype(np.float32))
+    colorVBO.bind()
+
+    print(vertVBO)
+    print(colorVBO)
+
+    mesh = ObjectMesh(vertVBO, colorVBO, gl.GL_TRIANGLES, indices)
+
+    min_v = vertices.min(axis=0)
+    max_v = vertices.max(axis=0)
+    collision = CollisionBox(glm.vec3(min_v), glm.vec3(max_v))
+
+    center = (min_v + max_v) / 2.0
+
+    obj = SceneObject(mesh, collision, center)
+
+    return obj
+
+
+def create_cube():
     cubeClrArray = np.array(
         [[0.0, 0.0, 0.0],
          [1.0, 0.0, 0.0],
@@ -42,7 +97,34 @@ def create_cube():
          2, 1, 5, 6,
          0, 3, 7, 4,
          7, 6, 5, 4])
-    mesh = ObjectMesh(vertVBO, colorVBO, cubeIdxArray, gl.GL_QUADS)
+    cubeVtxArray = np.array(
+        [[0.0, 0.0, 0.0],
+         [1.0, 0.0, 0.0],
+         [1.0, 1.0, 0.0],
+         [0.0, 1.0, 0.0],
+         [0.0, 0.0, 1.0],
+         [1.0, 0.0, 1.0],
+         [1.0, 1.0, 1.0],
+         [0.0, 1.0, 1.0]])
+    vertVBO = vbo.VBO(np.reshape(cubeVtxArray,
+                                 (1, -1)).astype(np.float32))
+    vertVBO.bind()
+    cubeEdgesIdxArray = np.array(
+        [0, 1,
+         1, 2,
+         2, 3,
+         3, 0,
+         0, 4,
+         1, 5,
+         2, 6,
+         3, 7,
+         4, 5,
+         5, 6,
+         6, 7,
+         7, 4]
+    )
+
+    mesh = ObjectMesh(vertVBO, colorVBO, gl.GL_QUADS, cubeIdxArray, cubeEdgesIdxArray)
     collision = CollisionBox(glm.vec3([0.0, 0.0, 0.0]), glm.vec3([1.0, 1.0, 1.0]))
     obj = SceneObject(mesh, collision, np.array([0.5, 0.5, 0.5]))
 
